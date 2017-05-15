99e5b3f339a4eb610b54a4256bdeedca2dc9d453
Adnan <adnan.tumunich@gmail.com>
2017-05-15 12:51:37 +0200

    Working AAT

--- client.rb
+++ client.rb
@@ -4,13 +4,19 @@ require 'json'
 require 'base64'
 require 'date'
 require 'net/http'
+require 'rest-client'
 
 configure do
   set :bind, '0.0.0.0'
-  set :port, '8080'
+  set :port, '4568'
 end
 
 enable :sessions
+
+$knownIdentities = {}
+$umaServer = 'http://testservice.gnu:4567'
+$client_id = "random_client_id"
+$client_secret = "lksajr327048olkxjf075342jldsau0958lkjds"
 
 get '/logout' do
   if (!session["user"].nil?)
@@ -29,6 +35,7 @@ end
 get '/' do
   #Step #1
   identity = session["user"]
+  p identity
 
   if (!identity.nil?)
     token = $knownIdentities[identity] #AAT
@@ -45,9 +52,10 @@ get '/' do
   redirect $umaServer+"/authorize?response_type=code&client_id=s6BhdRkqt3&state=xyz&redirect_uri=http%3A%2F%2Fexample.gnu%3A4568%2Fcb&scope=uma_authorization"
 end
 
+#callback URL
 get "/cb" do
     #Step #9
-    code = params[:code]
+    code = params['code']
     p code
     # POST /token HTTP/1.1
     # Host: server.example.com
@@ -56,4 +64,19 @@ get "/cb" do
 #
 #     grant_type=authorization_code&code=SplxlOBeZQQYbYS6WxSbIA
 #     &redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb
+
+    encoded_authorization = ["#{$client_id}:#{$client_secret}"].pack('m').delete("\r\n")
+    puts "user pass #{$client_id} #{$client_secret}"
+    r = RestClient::Request.execute(method: :post,
+                                    url: 'http://testservice.gnu:4567/token',
+                                    verify_ssl: false,
+                                    user: $client_id,
+                                    password: $client_secret,
+                                    headers: {Authorization: 'Basic #{encoded_authorization}',
+                                      content_type: 'json', params: {grant_type: 'authorization_code',code: code, redirect_uri: 'http%3A%2F%2Fexample%2Egnu%2Fcb'}})
+    response_body = JSON.parse(r.body)
+    puts "response aat #{response_body["access_token"]}"
+
+
+
 end
